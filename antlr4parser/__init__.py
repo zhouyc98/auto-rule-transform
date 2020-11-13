d0b541936f094c4917a0d2b72a7724453d2ae720
zhouyucheng <zhouyucheng98@outlook.com>
2020-11-13 15:40:33 +0800

    delete files

--- antlr4parser/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-
-from antlr4 import *
-from .RuleCheckTreeLexer import RuleCheckTreeLexer
-from .RuleCheckTreeParser import RuleCheckTreeParser
-from .RuleCheckTreeListener import RuleCheckTreeListener
-from .RuleCheckTreeVisitor import RuleCheckTreeVisitor
-from .RuleCheckTreeErrorListener import *
